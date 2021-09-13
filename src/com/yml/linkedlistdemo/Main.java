127c6247135b4dac76aadcf8deecd6ec0be496eb
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-13 19:17:00 +0530

    Added push method to insert elements at the front of the linked list and method to print elements of linkedList

--- src/com/yml/linkedlistdemo/Main.java
+++ src/com/yml/linkedlistdemo/Main.java
@@ -5,7 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to LinkedList program");
 		LinkedList linkedList = new LinkedList();
-
+		linkedList.push(70);
+		linkedList.push(30);
+		linkedList.push(56);
+		linkedList.print();
 	}
 
 }
