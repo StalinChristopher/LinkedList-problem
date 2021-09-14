dfb196ce40010bca0b9df1d7966ed206888c1e36
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-14 17:31:57 +0530

    Added SortedLinkedList class to sort the element when adding it to the linkedList

--- src/com/yml/linkedlistdemo/Main.java
+++ src/com/yml/linkedlistdemo/Main.java
@@ -12,27 +12,35 @@ public class Main {
 		/*Here linkedList of type Integer is created. Any type of LinkedList can be created since it is of
 		 *generic type
 		 */
-		LinkedList<Integer> linkedList = new LinkedList<Integer>();
+//		LinkedList<Integer> linkedList = new LinkedList<Integer>();
+//		
+//		linkedList.append(56);
+//		linkedList.append(30);
+//		linkedList.append(70);
+//		
+//		linkedList.insertAtPos(3, 10);
+//		
+//		linkedList.print();
+//		
+//		linkedList.popLast();
+//		
+//		System.out.println("\nElement 30 : "+ linkedList.search(30));
+//		
+//		linkedList.insertAfterValue(30, 40);
+//		
+//		linkedList.print();
+//		
+//		linkedList.remove(40);
+//		
+//		linkedList.print();
 		
-		linkedList.append(56);
-		linkedList.append(30);
-		linkedList.append(70);
+		SortedLinkedList sortedList = new SortedLinkedList<>();
+		sortedList.add(56);
+		sortedList.add(30);
+		sortedList.add(40);
+		sortedList.add(70);
+		sortedList.print();
 		
-		linkedList.insertAtPos(3, 10);
-		
-		linkedList.print();
-		
-		linkedList.popLast();
-		
-		System.out.println("\nElement 30 : "+ linkedList.search(30));
-		
-		linkedList.insertAfterValue(30, 40);
-		
-		linkedList.print();
-		
-		linkedList.remove(40);
-		
-		linkedList.print();
 	}
 
 }
